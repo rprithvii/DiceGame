7b4e33067185b915f691c890062bb41c92a6898b
Prithvi Raj <mail.prithvi@gmail.com>
2026-01-01 21:46:05 +0000

    Made attributes of the player class non public

--- src/player.py
+++ src/player.py
@@ -1,8 +1,18 @@
 class Player:
     def __init__(self, die, is_computer, counter = 5):
-        self.die = die
-        self.is_computer = is_computer
-        self.counter = counter
+        self._die = die
+        self._is_computer = is_computer
+        self._counter = counter
+
+    @property
+    def die(self):
+        return self._die
+    @property
+    def is_computer(self):
+        return self._is_computer
+    @property
+    def counter(self):
+        return self._counter
 
     def roll_die(self):
         self.die.roll()
